14d998406f04f6bd6013d902c28ec6dc37ba131e
wanghongliang <whlbayhax@163.com>
2021-10-14 18:48:19 +0800

    vue build separate

--- projectfba_fake/settings.py
+++ projectfba_fake/settings.py
@@ -62,7 +62,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         # 'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'DIRS': [os.path.join(BASE_DIR, 'projectfba_fake_fronted/dist')],
+        'DIRS': [os.path.join(BASE_DIR, 'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'projectfba_fake_fronted/dist/static')]
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'apps/home/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
